b70b46b6c2cebe775e23e41026b354a70377f5e6
Zacharia Ndinguri <zacthuku@gmail.com>
2025-05-14 08:40:53 +0300

    inheritance lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,12 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+   
     
-    def learn(self):
+    def learn(self, new_knowledge):
+        self.knowledge.append(new_knowledge)
         pass
